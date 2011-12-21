93942a37c20f2961b38d0d551bdfe54e724ba9f0
Graham Ballantyne <hello@grahamballantyne.com>
2011-12-20 21:57:14 -0800

    add basic functionality for proof of concept
    Signed-off-by: Graham Ballantyne <hello@grahamballantyne.com>

--- src/ca_sfu_icat_calscheduler.js
+++ src/ca_sfu_icat_calscheduler.js
@@ -9,7 +9,7 @@
 function ca_sfu_icat_calscheduler() { };
 
 ca_sfu_icat_calscheduler.prototype = new ZmZimletBase();
-ca_sfu_icat_calscheduler.prototype.constructor = ca_sfu_icat_template;
+ca_sfu_icat_calscheduler.prototype.constructor = ca_sfu_icat_calscheduler;
 
 ca_sfu_icat_calscheduler.prototype.init = function() {
 
@@ -18,9 +18,25 @@ ca_sfu_icat_calscheduler.prototype.init = function() {
 };
 
 ca_sfu_icat_calscheduler.prototype.singleClicked = function () {
-    this.displayZimletDialog({
-        
-    });
+    var dialog = this.createZimletDialog({
+		title     : 'SFU Connect',
+		width     : 500, 	// width of the inner canvas area
+		height    : '100%',	// height of the inner canvas area
+		popup     : true,	// if true, pops up immediately; if false, returns the dialog object for later use
+		ok_button : {
+			enabled : true,
+			string 	: 'Create New Appointment',
+			handler : [this, function() { dialog.popdown(); dialog.dispose(); this.openApptComposeTab(); }]
+		},
+		cancel_button : {
+			enabled : true,
+			string	: 'Cancel',
+			handler	: [null, function() { dialog.popdown(); dialog.dispose(); }]
+		},
+		override_tab_handler : true,
+		wrap_html: true,
+        html: '<p>The Calendar Scheduler has been moved to the appointment menu, now with more features. Create or edit an appointment, add attendees, and the Calendar Scheduler will automatically appear.</p><p><a href="#">Learn more about the new Calendar Scheduler</a></p><p>Click <strong>Create New Appointment</strong> button to start creating a new calendar appointment.</p><p><input type="checkbox" id="calsched_dontshowagain" name="calsched_dontshowagain" />Don&apos;t show this message again</p>'
+	});
 };
 
 ca_sfu_icat_calscheduler.prototype.doubleClicked = function () {
